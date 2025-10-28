ca0e106e0c6c0b439f7cbc8b1c5c5d9405f4c52c
Mazen Hamed <mazen@Mac.lan>
2025-10-28 14:06:57 +0800

    add: added search movie title and show list of movie in initial load by  default

--- src/stores/movieStore.ts
+++ src/stores/movieStore.ts
@@ -4,7 +4,6 @@ import { fetchMovies } from '@/api/movieService'
 interface Movie { Title: string; Year: string; imdbID: string }
 interface MovieState {
   movies: Movie[]
-  total: number
   title: string
   page: number
   totalPages: number
@@ -13,20 +12,21 @@ interface MovieState {
 export const useMovieStore = defineStore('movie', {
   state: (): MovieState => ({
     movies: [],
-    total: 0,
-    title: '',
+    title: 'Batman',
     page: 1,
     totalPages: 0
   }),
 
   actions: {
-    async searchMovies(title: string, page = 1) {
-      const res = await fetchMovies(title, page)
-      this.title = title
+    async loadMovies(page = 1) {
+      const res = await fetchMovies(this.title, page)
       this.page = page
       this.movies = res.data
-      this.total = res.total
       this.totalPages = res.total_pages
+    },
+    async searchMovies(title: string, page = 1) {
+      this.title = title
+      await this.loadMovies(page)
     }
   },
 
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container class="py-8">
     <SearchBar @search="search" />
-
     <MovieList :movies="store.movies" :loading="loading" />
 
     <v-pagination
@@ -16,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useMovieStore } from '../stores/movieStore'
 import SearchBar from '../components/SearchBar.vue'
 import MovieList from '../components/MovieList.vue'
@@ -25,16 +24,22 @@ const store = useMovieStore()
 const loading = ref(false)
 const page = ref(1)
 
+onMounted(async () => {
+  loading.value = true
+  await store.loadMovies()
+  loading.value = false
+})
+
 const search = async (query: string) => {
   loading.value = true
   page.value = 1
-  await store.searchMovies(query, page.value)
+  await store.searchMovies(query)
   loading.value = false
 }
 
 const changePage = async (newPage: number) => {
   loading.value = true
-  await store.searchMovies(store.title, newPage)
+  await store.loadMovies(newPage)
   loading.value = false
 }
 </script>
