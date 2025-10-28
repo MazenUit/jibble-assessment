24de007994b07b65a7c17e478b729feed76df60d
Mazen Hamed <mazen@Mac.lan>
2025-10-28 14:03:45 +0800

    add: added pagination

--- src/stores/movieStore.ts
+++ src/stores/movieStore.ts
@@ -2,18 +2,33 @@ import { defineStore } from 'pinia'
 import { fetchMovies } from '@/api/movieService'
 
 interface Movie { Title: string; Year: string; imdbID: string }
-interface MovieState { movies: Movie[]; total: number; title: string; page: number }
+interface MovieState {
+  movies: Movie[]
+  total: number
+  title: string
+  page: number
+  totalPages: number
+}
 
 export const useMovieStore = defineStore('movie', {
-  state: (): MovieState => ({ movies: [], total: 0, title: '', page: 1 }),
+  state: (): MovieState => ({
+    movies: [],
+    total: 0,
+    title: '',
+    page: 1,
+    totalPages: 0
+  }),
+
   actions: {
     async searchMovies(title: string, page = 1) {
+      const res = await fetchMovies(title, page)
       this.title = title
       this.page = page
-      const res = await fetchMovies(title, page)
       this.movies = res.data
       this.total = res.total
+      this.totalPages = res.total_pages
     }
   },
+
   persist: { storage: localStorage }
 })
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -4,11 +4,14 @@
 
     <MovieList :movies="store.movies" :loading="loading" />
 
-    <v-row justify="center" v-if="!store.movies.length && !loading">
-      <v-col cols="12" class="text-center text-grey">
-        <p>No movies found</p>
-      </v-col>
-    </v-row>
+    <v-pagination
+      v-if="store.totalPages > 1"
+      v-model="page"
+      :length="store.totalPages"
+      color="primary"
+      class="mt-6 d-flex justify-center"
+      @update:model-value="changePage"
+    />
   </v-container>
 </template>
 
@@ -20,10 +23,18 @@ import MovieList from '../components/MovieList.vue'
 
 const store = useMovieStore()
 const loading = ref(false)
+const page = ref(1)
 
 const search = async (query: string) => {
   loading.value = true
-  await store.searchMovies(query)
+  page.value = 1
+  await store.searchMovies(query, page.value)
+  loading.value = false
+}
+
+const changePage = async (newPage: number) => {
+  loading.value = true
+  await store.searchMovies(store.title, newPage)
   loading.value = false
 }
 </script>
